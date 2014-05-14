98b08aa463c7c57d6823dcb0cd724b62cedbb7ca
Nico Sebastian <sx.cologne@gmail.com>
2014-05-14 12:49:13 +0200

    Eingerichtet

---
+++ server.js
@@ -0,0 +1,15 @@
+/* GameBuddies Forum Software
+*
+* @author Nico Sebastian Schönenberg (@SxCologne) & Rafael Marques (@RafaMarques9)
+*
+* Server
+*/
+
+console.log('Module werden geladen...');
+//Pakete einbinden
+var express = require('express');
+var faye    = require('faye');
+var http    = require('http');
+var mongoDB = require('mongoskin');
+console.log('Module geladen.');
+
